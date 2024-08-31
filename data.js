9659da9c98539fe794f24e91b0a79deaaf469b1b
cosmetologydevaces <cosmetologydevaces@gmail.com>
2024-08-31 21:27:23 +0300

    upload

---
+++ data.js
@@ -0,0 +1,108 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-3",
+      "name": "3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 320,
+      "initialViewParameters": {
+        "yaw": 1.495167121624613,
+        "pitch": 0.41973760499142543,
+        "fov": 1.186967206414792
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9507668511126894,
+          "pitch": 0.1181266964075327,
+          "rotation": 0.7853981633974483,
+          "target": "1-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-1",
+      "name": "1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 320,
+      "initialViewParameters": {
+        "yaw": -1.5268153763668906,
+        "pitch": 0.034132407614619,
+        "fov": 1.186967206414792
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.335426098308929,
+          "pitch": 0.21208792390527798,
+          "rotation": 0,
+          "target": "2-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.4160976807827357,
+          "pitch": -0.2071913073437308,
+          "title": "Elkobesy",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "2-2",
+      "name": "2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 320,
+      "initialViewParameters": {
+        "yaw": -0.5145307524912788,
+        "pitch": 0.4596584501553522,
+        "fov": 1.186967206414792
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2413995292143412,
+          "pitch": 0.07504233886026057,
+          "rotation": 0.7853981633974483,
+          "target": "0-3"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
